f4fd540d638470ce84ebc89a60de080739971671
TheMasterof <marcel.plakolb@liweswt.at>
2020-03-20 11:20:43 +0100

    finish filat

--- programms/filat.c
+++ programms/filat.c
@@ -1,79 +1,72 @@
 //PLAKOLB Marcel
 #include <sys/stat.h>
-#include <time.h>
-#include <fcntl.h>
 #include <stdio.h>
+#include <time.h>
 
-int main(int argc, char const *argv[])
-{
-    const char *file = argv[1];
-
-    // Try to get info about file
-    struct stat file_stat;
-    // lstat requires gnu99 instead of c99 (isn't compliant w/ ANSI standard)
-    int stat_return = lstat(file, &file_stat);
-    if(stat_return < 0) {
-        printf("filat: cannot stat '%s'\n", file);
-        return -1;
+int main(int argc, char const *argv[]) {
+    if(argc != 2) {
+        printf("Please enter exactly one argument!\n");
+        return 0;
     }
+    printf("------------------- %s -------------------\n", argv[1]);
+    struct stat attributes;
 
-    // Print header
-    printf("-------------------");
-    printf(" %s ", file);
-    printf("------------------------\n");
-
-    // Print file/device type
+    int ret = lstat(argv[1], &attributes);
+    if(ret != 0) {
+        printf("Couldn't read attributes of file: %s\n", argv[1]);
+        return 0;
+    }
     printf("File type\t\t: ");
-    switch (file_stat.st_mode & S_IFMT) {
-        case S_IFREG:
-            puts("Regular file");
-            break;
-        case S_IFDIR:
-            puts("Directory");
-            break;
-        case S_IFCHR:
-            puts("Character Device");
-            break;
-        case S_IFBLK:
-            puts("Block device");
-            break;
-        case S_IFLNK:
-            puts("Symlink");
-            break;
-        case S_IFIFO:
-            puts("Pipe");
-            break;
-        case S_IFSOCK:
-            puts("Socket");
-            break;
-        default:
-            puts("? - Unknown");
+    switch (attributes.st_mode & S_IFMT) {
+    case S_IFBLK:
+        printf("Block special file\n");
+        break;
+    case S_IFCHR:
+        printf("Device special file\n");
+        break;
+    case S_IFDIR:
+        printf("Directory\n");
+        break;
+    case S_IFLNK:
+        printf("Symbolic link\n");
+        break;
+    case S_IFSOCK:
+        printf("Socket\n");
+        break;
+    case S_IFREG:
+        printf("Regular file\n");
+        break;
+    case S_IFIFO:
+        printf("FIFO\n");
+        break;
+    default:
+        printf("Unknown\n");
+        break;
     }
 
-    // Print privileges
     printf("Access privileges\t: ");
-    printf((S_ISDIR(file_stat.st_mode)) ? "d" : "-");
-    printf((file_stat.st_mode & S_IRUSR) ? "r" : "-");
-    printf((file_stat.st_mode & S_IWUSR) ? "w" : "-");
-    printf((file_stat.st_mode & S_IXUSR) ? "x" : "-");
-    printf((file_stat.st_mode & S_IRGRP) ? "r" : "-");
-    printf((file_stat.st_mode & S_IWGRP) ? "w" : "-");
-    printf((file_stat.st_mode & S_IXGRP) ? "x" : "-");
-    printf((file_stat.st_mode & S_IROTH) ? "r" : "-");
-    printf((file_stat.st_mode & S_IWOTH) ? "w" : "-");
-    printf((file_stat.st_mode & S_IXOTH) ? "x" : "-");
-    puts("");
+    printf( (attributes.st_mode & S_IRUSR) ? "r" : "-");
+    printf( (attributes.st_mode & S_IWUSR) ? "w" : "-");
+    printf( (attributes.st_mode & S_IXUSR) ? "x" : "-");
+    printf( (attributes.st_mode & S_IRGRP) ? "r" : "-");
+    printf( (attributes.st_mode & S_IWGRP) ? "w" : "-");
+    printf( (attributes.st_mode & S_IXGRP) ? "x" : "-");
+    printf( (attributes.st_mode & S_IROTH) ? "r" : "-");
+    printf( (attributes.st_mode & S_IWOTH) ? "w" : "-");
+    printf( (attributes.st_mode & S_IXOTH) ? "x\n" : "-\n");
 
-    // Print other details
-    printf("inode-Number\t\t: %ld\n", file_stat.st_ino);
-    printf("Dev. numbers\t\t: dev = %ld; rdev = %ld\n", file_stat.st_dev, file_stat.st_rdev);
-    printf("Links count\t\t: %ld\n", file_stat.st_nlink);
-    printf("UID\t\t\t: %d\n", file_stat.st_uid);
-    printf("GID\t\t\t: %d\n", file_stat.st_gid);
-    printf("File size\t\t: %ld\n", file_stat.st_size);
+    time_t a_time = attributes.st_atime;
+    time_t m_time = attributes.st_mtime;
+    time_t c_time = attributes.st_ctime;
 
-    // Print date-times
-    printf("Last access\t\t: %s", ctime(&file_stat.st_atime));
-    printf("Last modification\t: %s", ctime(&file_stat.st_mtime));
-    printf("Last inode change\t: %s", ctime(&file_stat.st_ctime));
+    printf("Inode-Number\t\t: %ld\n", attributes.st_ino);
+    printf("Device numbers:\t\t: dev = %ld; rdev = %ld\n", attributes.st_dev, attributes.st_rdev);
+    printf("Links count\t\t: %ld\n", attributes.st_nlink);
+    printf("UID\t\t\t: %d\n", attributes.st_uid);
+    printf("GID\t\t\t: %d\n", attributes.st_gid);
+    printf("File size\t\t: %ld\n", attributes.st_size);
+    printf("Last access\t\t: %s", ctime(&a_time));
+    printf("Last modification\t: %s", ctime(&m_time));
+    printf("Last inode change\t: %s", ctime(&c_time));
+    return 0;
 }
